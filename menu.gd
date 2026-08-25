0f6a187be02977f0344e6eff0f47c4dafc834d22
elud123 <e.lud.2010.emanuelli@gmail.com>
2026-08-25 15:11:43 -0300

    fiz o menu

---
+++ menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func get_tree().change_scene_to_file("res://cenas/jogo.tscn")
+ -> void:
+	pass # Replace with function body.
+
+
+func get_tree().quit() -> void:
+	pass # Replace with function body.
